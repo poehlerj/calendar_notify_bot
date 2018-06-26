ebb60bfa20b18f76dfcd3fa5ab3c5eb16fef58c5
Jonas Pöhler <jonas.poehler@online.de>
2018-06-26 15:55:15 +0200

    Deleted unused stuff

--- update_message_catalog.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-if [ -n "$1" ] && [ "$1" == "generate" ]; then
-    ./setup.py extract_messages --output-file calendar_bot/locales/messages.po
-    ./setup.py update_catalog -l de -i calendar_bot/locales/messages.po -o calendar_bot/locales/de/LC_MESSAGES/messages.po
-else
-    ./setup.py compile_catalog --directory calendar_bot/locales --locale de
-fi
